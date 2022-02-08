ecbf1415de52e4fbad8562f76dc6eb698cd01840
Dike Nnamaka <nnamaka7@gmail.com>
2022-02-08 17:21:30 +0100

    opened a new file

---
+++ show_webcam_cv.py
@@ -0,0 +1,27 @@
+
+import cv2
+
+#img = cv2.imread("lena.jpg",-1)
+
+#cv2.imshow('smy',img)
+#cv2.waitKey(0)
+#cv2.destroyAllwindows()
+
+# 0 is first camera, 1 is second camera , 3 is third
+
+cap = cv2.VideoCapture(0)
+# display video from video file
+# cap = cv2.VideoCapture("name.mp4")
+
+while True:
+    ret, frame = cap.read()
+
+    # convert color output to grey
+    #grey = cv2.cvtoColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('webcam', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
