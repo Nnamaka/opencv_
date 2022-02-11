de7808d1ef2025009530b2c93bf1f4126ec5b621
Dike Nnamaka <nnamaka7@gmail.com>
2022-02-11 23:28:32 +0100

    draw road lane lines

---
+++ hough_transform.py
@@ -0,0 +1,29 @@
+
+# 6:36:00
+
+# Hough is a popular technique to detect any shape, if you can 
+# represent that shape in a mathematical form. it can detect the shape
+# even if it is broken or distorted a little bit
+# you can use hough method for lane lines detection on the road
+
+# lets create a lane detection system
+# 7:13:00
+
+import matplotlib.pylab as plt 
+import numpy as np 
+import cv2 as cv
+
+
+# designing region of interest and detecting the lane lines
+
+
+
+
+
+
+# draw road lane lines 
+
+
+
+# detecting circles using hough circle transform
+# 8:00:00
